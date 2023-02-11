874ec14b2c9dc56b1aaf8cf508c11c8fe2f87989
BLACKHENZ29 <ayehenz29@gmail.com>
2023-02-11 07:53:04 +0000

    Created Model For Registering An Evtol

---
+++ models/evtolReg.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const RegSchema = new mongoose.Schema({
+    serialNo:{
+        type:String,
+        required:true
+    },
+    model:{
+        type:String,
+        required:true
+    },
+    weight:{
+        type:Number,
+        required:true
+    },
+    battery:{
+        type:Number,
+        required:true
+    },
+    state:{
+        type:String,
+        required:true
+    }
+},
+{
+    timestamps:true,
+    toJSON:{virtuals:true}
+}
+)
+
+const evReg = mongoose.model('evReg',RegSchema);
+
+export default evReg;
