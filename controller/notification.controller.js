81725e241152c680f95fdea9e0c6b23684dd7c06
BLACKHENZ29 <ayehenz29@gmail.com>
2024-01-11 11:34:45 +0100

    Request Sent Notification Refactor

--- controller/notification.controller.js
+++ controller/notification.controller.js
@@ -1,17 +1,14 @@
 import Messages from './../models/notification.model';
 
-export const requestSent = async(req, res)=>{
+export const requestSent = async(requestId)=>{
     try {
         const newMessage = new Messages({
           title: "Request Created",
           body: `Your request with ID ${requestId} has been successfully created.`,
           status: "Unread"
         });
-        await newMessage.save();
-        res.status(201).json({
-          message: "Notification created successfully",
-          notification: newMessage
-        });
+        const savedMessage = await newMessage.save();
+        return savedMessage._id;
       } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Failed to create notification" });
