e801824e1d04b14be4635a74e35b8ff4d4f0c7f2
kingsleyhenz <ayehenz29@gmail.com>
2026-01-17 11:00:00 +0100

    migrate Device and Request models to TypeScript

---
+++ src/models/device.model.ts
@@ -0,0 +1,54 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { DeviceState } from '../typings/enums.js';
+
+export interface IDevice extends Document {
+    serialNo: string;
+    model: string;
+    weight: number;
+    battery: number;
+    state: DeviceState;
+    isBooked?: mongoose.Types.ObjectId;
+    deliveries: mongoose.Types.ObjectId[];
+}
+
+const DeviceSchema = new Schema<IDevice>({
+    serialNo: {
+        type: String,
+        required: true
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: true
+    },
+    battery: {
+        type: Number,
+        required: true
+    },
+    state: {
+        type: String,
+        enum: Object.values(DeviceState),
+        default: DeviceState.IDLE,
+    },
+    isBooked: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Request"  
+    },
+    deliveries: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Request"
+        }
+    ]
+},
+{
+    timestamps: true,
+    toJSON: { virtuals: true }
+});
+
+const Device = mongoose.model<IDevice>('Device', DeviceSchema);
+
+export default Device;
