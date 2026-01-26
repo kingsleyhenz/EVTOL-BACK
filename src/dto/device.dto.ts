044f9042f138ce3d1ab1af4f019cca45978b36ea
kingsleyhenz <ayehenz29@gmail.com>
2026-01-26 11:00:00 +0100

    refactor Device DTO to use class-validator decorators

--- src/dto/device.dto.ts
+++ src/dto/device.dto.ts
@@ -1,16 +1,46 @@
-import { DeviceState } from '../typings/enums';
+import { IsString, IsNumber, IsNotEmpty, IsEnum, IsOptional, Max, Min } from 'class-validator';
+import { DeviceState } from '../typings/enums.ts';
 
-export interface CreateDeviceDto {
-  serialNo: string;
-  model: string;
-  weight: number;
-  battery: number;
+export class CreateDeviceDto {
+  @IsString()
+  @IsNotEmpty()
+  serialNo!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  model!: string;
+
+  @IsNumber()
+  @Max(500)
+  @Min(1)
+  weight!: number;
+
+  @IsNumber()
+  @Max(100)
+  @Min(0)
+  battery!: number;
+
+  @IsEnum(DeviceState)
+  @IsOptional()
   state?: DeviceState;
 }
 
-export interface UpdateDeviceDto {
+export class UpdateDeviceDto {
+  @IsString()
+  @IsOptional()
   model?: string;
+
+  @IsNumber()
+  @IsOptional()
+  @Max(500)
   weight?: number;
+
+  @IsNumber()
+  @IsOptional()
+  @Max(100)
   battery?: number;
+
+  @IsEnum(DeviceState)
+  @IsOptional()
   state?: DeviceState;
 }
