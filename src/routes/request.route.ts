3ee0e9457037c906fa25126a8dbaa980b0883f54
kingsleyhenz <ayehenz29@gmail.com>
2026-01-29 11:00:00 +0100

    update request routes to use RequestController instance

--- src/routes/request.route.ts
+++ src/routes/request.route.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import RequestController from '../controller/request.controller.ts';
 import { isLoggedIn } from '../middleware/isLoggedIn.ts';
+import { validateDto } from '../middleware/validate.ts';
+import { CreateRequestDto } from '../dto/request.dto.ts';
 
 const requestRoute = express.Router();
 
-requestRoute.post("/send-request", isLoggedIn, RequestController.makeRequest);
-requestRoute.get("/all-requests", isLoggedIn, RequestController.getAllRequests);
-requestRoute.get("/get/:requestId", isLoggedIn, RequestController.getRequestById);
-requestRoute.get("/my-requests", isLoggedIn, RequestController.getMyRequests);
-requestRoute.put("/accept/:requestId", isLoggedIn, RequestController.acceptRequest);
+requestRoute.post('/create', isLoggedIn, validateDto(CreateRequestDto), RequestController.makeRequest);
+requestRoute.get('/all', isLoggedIn, RequestController.getAllRequests);
+requestRoute.get('/my-requests', isLoggedIn, RequestController.getMyRequests);
+requestRoute.get('/:requestId', isLoggedIn, RequestController.getRequestById);
+requestRoute.patch('/:requestId/accept', isLoggedIn, RequestController.acceptRequest);
 
 export default requestRoute;
