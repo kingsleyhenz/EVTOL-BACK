1653262256bd99c392b4278e58e4993ac6601862
kingsleyhenz <ayehenz29@gmail.com>
2026-01-29 10:00:00 +0100

    update user routes to use UserController instance

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,13 @@
-import express from "express";
+import express from 'express';
 import UserController from '../controller/user.controller.ts';
 import { isLoggedIn } from '../middleware/isLoggedIn.ts';
+import { validateDto } from '../middleware/validate.ts';
+import { CreateUserDto, LoginDto } from '../dto/user.dto.ts';
 
 const userRoute = express.Router();
 
-userRoute.post('/create-account', UserController.register);
-userRoute.post('/login', UserController.login);
+userRoute.post('/create-account', validateDto(CreateUserDto), UserController.register);
+userRoute.post('/login', validateDto(LoginDto), UserController.login);
 userRoute.get('/my-profile', isLoggedIn, UserController.getProfile);
 
 export default userRoute;
