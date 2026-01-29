94fad24757fbeb7cced5c4affe4a3c94e2184d4e
kingsleyhenz <ayehenz29@gmail.com>
2026-01-29 12:00:00 +0100

    update notification routes to use NotificationController instance

--- src/routes/notification.route.ts
+++ src/routes/notification.route.ts
@@ -1,10 +1,10 @@
-import express from "express";
-import NotificationController from "../controller/notification.controller.ts";
+import express from 'express';
+import NotificationController from '../controller/notification.controller.ts';
 import { isLoggedIn } from '../middleware/isLoggedIn.ts';
 
 const messageRoute = express.Router();
 
-messageRoute.get("/", isLoggedIn, NotificationController.getMyNotifications);
-messageRoute.get("/get/:notificationId", isLoggedIn, NotificationController.getNotificationById);
+messageRoute.get('/', isLoggedIn, NotificationController.getMyNotifications);
+messageRoute.get('/:notificationId', isLoggedIn, NotificationController.getNotificationById);
 
 export default messageRoute;
